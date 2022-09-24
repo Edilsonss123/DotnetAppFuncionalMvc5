ed0d624db1cd49b198092e54f100fa6c9ef7beee
edilsonss123 <edilson016@outlook.com>
2022-09-24 16:07:46 -0300

    primeiro commit

---
+++ AppMvc/Controllers/AlunosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AppMvc.Models;
+
+namespace AppMvc.Controllers
+{
+    [Authorize]
+    public class AlunosController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Alunos
+        [HttpGet]
+        [OutputCache(Duration =60)]
+        [Route("listar-alunos")]
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Alunos.ToListAsync());
+        }
+
+        // GET: Alunos/Details/5
+        [HttpGet]
+        [Route("aluno-detalhe/{id:int}")]
+        public async Task<ActionResult> Details(int id)
+        {
+            var aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        // GET: Alunos/Create
+        [HttpGet]
+        [Route("novo-aluno")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alunos/Create
+        [HttpPost]
+        [Route("novo-aluno")]
+        [HandleError(ExceptionType = typeof(NullReferenceException), View = "Erro")]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Nome,Email,CPF,Ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                aluno.DataMatricula = DateTime.Now;
+                db.Alunos.Add(aluno);
+                await db.SaveChangesAsync();
+                TempData["Mensagem"] = "Aluno cadastro com sucesso!";
+                return RedirectToAction("Index");
+            }
+
+            return View(aluno);
+        }
+
+        // GET: Alunos/Edit/5
+        [HttpGet]
+        [Route("editar-aluno/{id:int}")]
+        public async Task<ActionResult> Edit(int id)
+        {
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Mensagem = "Edilson lindo";
+            return View(aluno);
+        }
+
+        // POST: Alunos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("editar-aluno/{id:int}")]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Nome,Email,CPF,Ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aluno).State = EntityState.Modified;
+                db.Entry(aluno).Property(a => a.DataMatricula).IsModified = false;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(aluno);
+        }
+
+        [HttpGet]
+        [Route("excluir-aluno/{id:int}")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        // POST: Alunos/Delete/5
+        [HttpPost]
+        [Route("excluir-aluno/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            db.Alunos.Remove(aluno);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AppMvc/Models/IdentityModels.cs
@@ -0,0 +1,46 @@
+﻿using AppMvc.Models;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AppMvc.Models
+{
+    // É possível adicionar dados do perfil do usuário adicionando mais propriedades na sua classe ApplicationUser, visite https://go.microsoft.com/fwlink/?LinkID=317594 para obter mais informações.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Observe que o authenticationType deve corresponder àquele definido em CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Adicionar declarações de usuário personalizado aqui
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Aluno>().ToTable("Alunos");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
